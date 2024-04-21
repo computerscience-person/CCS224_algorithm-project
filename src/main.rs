84fcef9331d964c03d7d349b692b33ddb220c423
Oliver Ladores <oliver.ladores@wvsu.edu.ph>
2024-04-22 03:48:02 +0800

    Added capability to read from an arbitrary directory.
    Signed-off-by: Oliver Ladores <oliver.ladores@wvsu.edu.ph>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
 use std::error::Error;
 use std::path::PathBuf;
+use std::fs::{self, DirEntry};
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
     #[arg(short = 'd', long, value_name = "DIRECTORY")]
@@ -11,8 +12,14 @@ struct Cli {
     searcher: String,
 }
 
+impl Cli {
+    fn read_dir(&self) -> Result<Vec<Result<DirEntry, std::io::Error>>, std::io::Error> {
+        let files: Vec<_> = fs::read_dir(self.search_dir.clone().unwrap_or(".".into()))?.collect();
+        Ok(files)
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
-
     Ok(())
 }
